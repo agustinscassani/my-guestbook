66e9a90a245dcf871b6a4c065e4ae9cb6ffb10b1
Agustin Cassani <agustinscassani@gmail.com>
2024-01-11 18:11:02 -0300

    Check normal button with form status

--- app/ui/form.tsx
+++ app/ui/form.tsx
@@ -31,7 +31,6 @@ export default function Form() {
       <button
         className="flex items-center justify-center absolute right-1 top-1 px-2 py-1 font-medium h-8 bg-neutral-200 dark:bg-neutral-700 text-neutral-900 dark:text-neutral-100 rounded w-16"
         disabled={pending}
-        type="submit"
       >
         {pending ? 'Signing' : 'Sign'}
       </button>
