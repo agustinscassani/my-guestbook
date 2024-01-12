508edcd00fc94ca8d40d0da5792b7f6524d1ce3a
Agustin Cassani <agustinscassani@gmail.com>
2024-01-12 10:55:35 -0300

    Submit button with pending status fixed

--- app/ui/form.tsx
+++ app/ui/form.tsx
@@ -5,9 +5,22 @@ import { useFormStatus } from 'react-dom'
 
 import { saveGuestbookEntry } from '@/app/actions/guestbook'
 
+function SubmitButton() {
+  const { pending } = useFormStatus()
+
+  return (
+    <button
+      className='flex items-center justify-center absolute right-1 top-1 px-2 py-1 font-medium h-8 bg-neutral-200 dark:bg-neutral-700 text-neutral-900 dark:text-neutral-100 rounded w-16'
+      disabled={pending}
+      type='submit'
+    >
+      Sign
+    </button>
+  )
+}
+
 export default function Form() {
   const formRef = useRef<HTMLFormElement>(null)
-  const { pending } = useFormStatus()
 
   return (
     <form
@@ -17,24 +30,16 @@ export default function Form() {
       }}
       className='relative max-w-[500px]'
       ref={formRef}
-      style={{ opacity: !pending ? 1 : 0.7 }}
     >
       <input
         aria-label='Your message'
         className='pl-4 pr-32 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-neutral-300 rounded-md bg-gray-100 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100'
-        disabled={pending}
         name='entry'
         placeholder='Your message...'
         required
         type='text'
       />
-      <button
-        className='flex items-center justify-center absolute right-1 top-1 px-2 py-1 font-medium h-8 bg-neutral-200 dark:bg-neutral-700 text-neutral-900 dark:text-neutral-100 rounded w-16'
-        disabled={pending}
-        type='submit'
-      >
-        Sign
-      </button>
+      <SubmitButton />
     </form>
   )
 }
